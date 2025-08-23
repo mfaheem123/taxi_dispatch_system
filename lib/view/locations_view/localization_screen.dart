49e417fa0f8c946d5e1b5b6c9f3bf78296ebf3bd
Hubaib Ihsan <hubaibihsan8833@gmail.com>
2025-08-23 05:36:29 -0700

    localization screen navigation adding

--- lib/view/locations_view/localization_screen.dart
+++ lib/view/locations_view/localization_screen.dart
@@ -12,164 +12,159 @@ class LocalizationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          double screenWidth = constraints.maxWidth;
-          double tableWidthFactor =
-          screenWidth < 600 ? 0.95 : screenWidth < 1000 ? 0.7 : 0.5;
-          return Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const Text(
-                      "LOCALIZATION",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double screenWidth = constraints.maxWidth;
+        double tableWidthFactor =
+        screenWidth < 600 ? 0.95 : screenWidth < 1000 ? 0.7 : 0.5;
+        return Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text(
+                    "LOCALIZATION",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  // const SizedBox(width: 10),
+                  IconButton(
+                    onPressed: () => _showAddDialog(context),
+                    icon: const Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                    style: IconButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      padding: const EdgeInsets.all(4),
+                      minimumSize: const Size(28, 28),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6),
                       ),
                     ),
-                    // const SizedBox(width: 10),
-                    IconButton(
-                      onPressed: () => _showAddDialog(context),
-                      icon: const Icon(
-                        Icons.add,
-                        color: Colors.white,
-                        size: 18,
-                      ),
-                      style: IconButton.styleFrom(
-                        backgroundColor: Colors.green,
-                        padding: const EdgeInsets.all(4),
-                        minimumSize: const Size(28, 28),
-                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
+            ),
 
-              // Table
-              Expanded(
-                child: GetBuilder<LocationController>(
-                    builder: (controller){
-                      return SingleChildScrollView(
-                        scrollDirection: Axis.vertical,
-                        child: Align(
-                          alignment: Alignment.topCenter,
-                          child: FractionallySizedBox(
-                            widthFactor: tableWidthFactor, // responsive width
-                            child: Table(
-                              border: const TableBorder(
-                                horizontalInside:
-                                BorderSide(width: 0.5, color: Colors.grey),
-                                verticalInside:
-                                BorderSide(width: 0.5, color: Colors.grey),
-                                top: BorderSide(width: 0.5, color: Colors.grey),
-                                bottom: BorderSide(width: 0.5, color: Colors.grey),
-                                left: BorderSide(width: 0.5, color: Colors.grey),
-                                right: BorderSide(width: 0.5, color: Colors.grey),
-                              ),
-                              columnWidths: const {
-                                0: FlexColumnWidth(3),
-                                1: FlexColumnWidth(1),
-                              },
+            // Table
+            GetBuilder<LocationController>(
+                builder: (controller){
+                  return SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Align(
+                      alignment: Alignment.topCenter,
+                      child: FractionallySizedBox(
+                        widthFactor: tableWidthFactor, // responsive width
+                        child: Table(
+                          border: const TableBorder(
+                            horizontalInside:
+                            BorderSide(width: 0.5, color: Colors.grey),
+                            verticalInside:
+                            BorderSide(width: 0.5, color: Colors.grey),
+                            top: BorderSide(width: 0.5, color: Colors.grey),
+                            bottom: BorderSide(width: 0.5, color: Colors.grey),
+                            left: BorderSide(width: 0.5, color: Colors.grey),
+                            right: BorderSide(width: 0.5, color: Colors.grey),
+                          ),
+                          columnWidths: const {
+                            0: FlexColumnWidth(3),
+                            1: FlexColumnWidth(1),
+                          },
+                          children: [
+                            const TableRow(
+                              decoration:
+                              BoxDecoration(color: Color(0xFFE0E0E0)),
                               children: [
-                                const TableRow(
-                                  decoration:
-                                  BoxDecoration(color: Color(0xFFE0E0E0)),
-                                  children: [
-                                    Padding(
-                                      padding: EdgeInsets.symmetric(
-                                          vertical: 14, horizontal: 20),
-                                      child: Align(
-                                        alignment: Alignment.centerLeft,
-                                        child: Text(
-                                          "POSTCODE",
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 15,
-                                            color: Colors.black87,
-                                          ),
-                                        ),
+                                Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 14, horizontal: 20),
+                                  child: Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                      "POSTCODE",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15,
+                                        color: Colors.black87,
                                       ),
                                     ),
-                                    Padding(
-                                      padding: EdgeInsets.symmetric(
-                                          vertical: 14, horizontal: 30),
-                                      child: Align(
-                                        alignment: Alignment.centerRight,
-                                        child: Text(
-                                          "ACTIONS",
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 15,
-                                            color: Colors.black87,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
+                                  ),
                                 ),
-
-                                // Data rows
-                                ...controller.postcodes.asMap().entries.map((entry) {
-                                  final postcode = entry.value;
-
-                                  return TableRow(
-                                    decoration: const BoxDecoration(
-                                      color: Colors.white,
+                                Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 14, horizontal: 30),
+                                  child: Align(
+                                    alignment: Alignment.centerRight,
+                                    child: Text(
+                                      "ACTIONS",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15,
+                                        color: Colors.black87,
+                                      ),
                                     ),
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                            vertical: 9, horizontal: 20),
-                                        child: Align(
-                                          alignment: Alignment.centerLeft,
-                                          child: Text(
-                                            postcode.code,
-                                            style: const TextStyle(
-                                              fontSize: 14,
-                                              color: Colors.black87,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                            horizontal: 30),
-                                        child: Align(
-                                          alignment: Alignment.centerRight,
-                                          child: IconButton(
-                                            icon: const Icon(Icons.delete,
-                                                color: Colors.red),
-                                            splashRadius: 20,
-                                            onPressed: () =>
-                                                controller.removePostcode(postcode),
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  );
-                                }),
+                                  ),
+                                ),
                               ],
                             ),
-                          ),
+
+                            // Data rows
+                            ...controller.postcodes.asMap().entries.map((entry) {
+                              final postcode = entry.value;
+
+                              return TableRow(
+                                decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 9, horizontal: 20),
+                                    child: Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text(
+                                        postcode.code,
+                                        style: const TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.black87,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 30),
+                                    child: Align(
+                                      alignment: Alignment.centerRight,
+                                      child: IconButton(
+                                        icon: const Icon(Icons.delete,
+                                            color: Colors.red),
+                                        splashRadius: 20,
+                                        onPressed: () =>
+                                            controller.removePostcode(postcode),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              );
+                            }),
+                          ],
                         ),
-                      );
-                    }),
-              ),
-            ],
-          );
-        },
-      ),
+                      ),
+                    ),
+                  );
+                }),
+          ],
+        );
+      },
     );
   }
 
