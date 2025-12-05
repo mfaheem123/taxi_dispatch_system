b30387e2a9827a626becfba7a2e0444aed4674e0
Hubaib Ihsan <hubaibihsan8833@gmail.com>
2025-12-05 22:47:27 +0500

    change

--- lib/alert/restrict_drivers_alert.dart
+++ lib/alert/restrict_drivers_alert.dart
@@ -51,7 +51,7 @@ class _RestrictDriversAlertState extends State<RestrictDriversAlert> {
               height: 350,
               width: 450,
             padding: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
-            child: Column(
+            child: controller.allDriverData == null?SizedBox.shrink(): Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
 
