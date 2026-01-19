b3cc98daa0046b2da5b1cf6c0d6761ada4601070
OwaisAhmed09 <owaissikander09@gmail.com>
2026-01-19 18:14:10 +0500

    pc change commit

--- lib/view/booking_view/controller.dart
+++ lib/view/booking_view/controller.dart
@@ -3,8 +3,11 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
+import '../../component/networks/api.dart';
 import '../dashboard_view/Controller/dashboard_controller.dart';
+import '../dashboard_view/models/dashboard_table_model.dart';
 
 class BookingController extends GetxController{
 
@@ -32,6 +35,39 @@ class BookingController extends GetxController{
 
 
   ///>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>todo complete booking functionality
+
+
+
+
+/// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Pre Booking
+
+  DashboardTableModel? dashboardTableModelData;
+  RxInt dashboardTableCurrentPage = 1.obs;
+  RxInt dashboardTableTotalPages = 1.obs;
+  final int dashboardTableLimit = 20;
+  final int tableId = 2;
+  getDashboardTableData() async{
+    var response = await Api().get("bookings/getbytabs/${tableId}",
+        queryParameters: {
+          "page": dashboardTableCurrentPage.value,
+          "limit": dashboardTableLimit,
+        }
+    );
+    if(response.statusCode ==200){
+      dashboardTableModelData = DashboardTableModel.fromJson(response.data);
+      dashboardTableTotalPages.value = dashboardTableModelData!.total!;
+      update();
+    }
+  }
+
+  void dashboardTablePageChange(int page) {
+    dashboardTableCurrentPage.value = page;
+    getDashboardTableData();
+  }
+
+
+
+
 
 
 }
