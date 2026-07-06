3cd74c704dd9e8fa396d3a7764146a17b35a36b4
Owais Sikander <119361512+OwaisAhmed09@users.noreply.github.com>
2026-07-07 00:02:03 +0500

    push all done

--- lib/view/dashboard_view/dashboard/drivers.dart
+++ lib/view/dashboard_view/dashboard/drivers.dart
@@ -424,7 +424,7 @@ class _DriversViewState extends State<DriversView> {
                                             driver.bookingStatus == "Arrived"?Colors.yellow:
                                             driver.bookingStatus == "On Route"?Colors.red:
                                             driver.bookingStatus == "STC"?Colors.blue:
-                                            // driver.driverStatus == "On Break"? Colors.red:
+                                            driver.driverStatus == "On Break"? Colors.red:
                                             Colors.green,
                                           borderRadius: const BorderRadius.all(Radius.circular(4)),
                                         ),
