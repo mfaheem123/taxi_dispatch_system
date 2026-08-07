8a94011bb33c3f40762012a8a2946a707ab13dea
Hubaib <hubaibihsan8833@gmail.com>
2026-08-07 11:04:25 +0500

    url

--- lib/component/networks/url.dart
+++ lib/component/networks/url.dart
@@ -11,8 +11,8 @@ abstract class BaseConfig {
 class DevConfig implements BaseConfig {
   @override
   // String get baseUrl => "http://192.168.110.5:5000/api/";
-  // String get baseUrl => "https://www.nexustechnologys.com/api/";
-  String get baseUrl => "http://158.220.92.206:5000/api/";
+  String get baseUrl => "https://www.nexustechnologys.com/api/";
+  // String get baseUrl => "http://158.220.92.206:5000/api/";
 
 
   @override
@@ -23,8 +23,8 @@ class DevConfig implements BaseConfig {
 
   @override
   // String get apiUrl => "http://192.168.110.5:5000/api/";
-  // String get apiUrl => "https://www.nexustechnologys.com/api/";
-  String get apiUrl => "http://158.220.92.206:5000/api/";
+  String get apiUrl => "https://www.nexustechnologys.com/api/";
+  // String get apiUrl => "http://158.220.92.206:5000/api/";
 
   @override
   // String get socketUrl => "ws://192.168.5.251:8086/com";
@@ -35,8 +35,8 @@ class DevConfig implements BaseConfig {
 class ProductionConfig implements BaseConfig {
   @override
   // String get baseUrl => "http://192.168.110.5:5000/api/";
-  // String get baseUrl => "https://www.nexustechnologys.com/api/";
-  String get baseUrl => "http://158.220.92.206:5000/api/";
+  String get baseUrl => "https://www.nexustechnologys.com/api/";
+  // String get baseUrl => "http://158.220.92.206:5000/api/";
 
   @override
   String get imageUrl => "http://apis.bloodlines.info/public";
@@ -46,13 +46,13 @@ class ProductionConfig implements BaseConfig {
 
   @override
   // String get apiUrl => "http://192.168.110.5:5000/api/";
-  // String get apiUrl => "https://www.nexustechnologys.com/api/";
-  String get apiUrl => "http://158.220.92.206:5000/api/";
+  String get apiUrl => "https://www.nexustechnologys.com/api/";
+  // String get apiUrl => "http://158.220.92.206:5000/api/";
 
   @override
   // String get socketUrl => "ws://192.168.110.5:5000/websocket";
-  // String get socketUrl => "wss://www.nexustechnologys.com/websocket";
-  String get socketUrl => "ws://158.220.92.206:5000/websocket";
+  String get socketUrl => "wss://www.nexustechnologys.com/websocket";
+  // String get socketUrl => "ws://158.220.92.206:5000/websocket";
 
 
 // String get mapKey => "AIzaSyDLtchj3AddQGK3mlMgqA6HKbLQlEkEa38";
