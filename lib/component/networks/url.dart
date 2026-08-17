aef928690c0218dd91fa796e77fd5e15971a1b15
Hubaib <hubaibihsan8833@gmail.com>
2026-08-17 15:56:39 +0500

    login

--- lib/component/networks/url.dart
+++ lib/component/networks/url.dart
@@ -11,8 +11,8 @@ abstract class BaseConfig {
 class DevConfig implements BaseConfig {
   @override
   // String get baseUrl => "http://192.168.110.5:5000/api/";
-  String get baseUrl => "https://www.nexustechnologys.com/api/";
-  // String get baseUrl => "http://158.220.92.206:5000/api/";
+  // String get baseUrl => "https://www.nexustechnologys.com/api/";
+  String get baseUrl => "http://158.220.92.206:5000/api/";
 
 
   @override
@@ -23,13 +23,13 @@ class DevConfig implements BaseConfig {
 
   @override
   // String get apiUrl => "http://192.168.110.5:5000/api/";
-  String get apiUrl => "https://www.nexustechnologys.com/api/";
-  // String get apiUrl => "http://158.220.92.206:5000/api/";
+  // String get apiUrl => "https://www.nexustechnologys.com/api/";
+  String get apiUrl => "http://158.220.92.206:5000/api/";
 
   @override
   // String get socketUrl => "ws://192.168.5.251:8086/com";
-String get socketUrl => "wss://www.nexustechnologys.com/websocket";
-  // String get socketUrl => "ws://158.220.92.206:5000/websocket";
+// String get socketUrl => "wss://www.nexustechnologys.com/websocket";
+  String get socketUrl => "ws://158.220.92.206:5000/websocket";
 }
 
 class ProductionConfig implements BaseConfig {
